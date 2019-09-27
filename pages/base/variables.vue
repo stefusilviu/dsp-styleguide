e9f982fe85bebc8cb02d300013ff28b3afd4cd5a
Silviu Stefu <silviu.stefu@endava.com>
2019-09-27 16:53:49 +0300

    Removed unused file colors.vue and added Variables content

--- pages/base/variables.vue
+++ pages/base/variables.vue
@@ -3,14 +3,23 @@
 
     <h2 class="page-title">Variables</h2>
     <div class="page-description mb-30">
-      <p>Short description of the component</p>
+      <p>We're using SCSS variables in order to keep property values in sync and more smooth later edits.</p>
     </div>
+
+    <el-card>
+      <el-row :gutter="20">
+      <el-col :sm="24" :md="24">
+        <prism language="css" :lineNumbers="true" :readonly="true" :code="css"></prism>
+      </el-col>
+    </el-row>
+    </el-card>
 
   </app-page>
 </template>
 
 <script>
   import AppPage from '@/components/AppPage'
+  import css from "!!raw-loader!@/assets/scss/stg-design/base/_variables.scss";
 
   export default {
     layout: 'page-layout',
@@ -20,6 +29,11 @@
     head () {
       return {
         title: 'Variables | Base',
+      }
+    },
+    data() {
+      return {
+        css
       }
     }
   }
