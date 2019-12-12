e44e74a86e9c1258bfebd9cb6b24f51857db2e3d
Silviu Stefu <silviu.stefu@endava.com>
2019-12-12 12:14:00 +0200

    New build for release v1.0.10

--- dist/js/stg-design.js
+++ dist/js/stg-design.js
@@ -124,6 +124,38 @@ window.addEventListener('load', initStgSelect)
 /* If the user clicks anywhere outside the select box,
 then close all select boxes: */
 document.addEventListener("click", closeAllSelect);
+function initCardToggle() {
+  var cards, parent;
+
+  cards = document.getElementsByClassName("card-toggle-button");
+
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", function (e) {
+      parent = this.parentNode.parentNode;
+      toggle(cards[i], parent)
+    });
+  }
+}
+
+function toggle(toggleBtn, card) {
+  var content = card.getElementsByClassName("card-expandable-content")[0];
+  if (!card.classList.contains('open')) {
+    // card.classList.add('open');
+    // content.style.height = "auto";
+    // var height = content.offsetHeight + "px";
+    // content.style.height = "0px";
+    // setTimeout(function() {
+    //   content.style.height = height;
+    // }, 0);
+  } else {
+    content.style.height = "0px";
+    content.addEventListener('transitionend', function() {
+      card.classList.remove('open');
+    }, { once: true });
+  }
+}
+
+window.addEventListener('load', initCardToggle);
 (function(){
     window.addEventListener('DOMContentLoaded', function() {
         var ccInputs = document.getElementsByClassName('cc-input');
