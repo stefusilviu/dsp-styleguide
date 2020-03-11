afbd1f2478a1434b166013e64fac2ab5f808e73f
Vlad Neacsu <vneacsu@mcclatchyservices.com>
2020-03-11 11:11:06 +0200

    Fix for expandable cards.
    * Add correct examples for expandable cards
    * Add extra design for cards

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -1,14 +1,20 @@
 <template>
-  <el-menu class="side-menu" :collapse="sidebarCollapsed || isMobile" :router="true" :unique-opened="true" :default-active="$nuxt.$route.path" active-text-color="#04816A">
-
+  <el-menu
+    class="side-menu"
+    :collapse="sidebarCollapsed || isMobile"
+    :router="true"
+    :unique-opened="true"
+    :default-active="$nuxt.$route.path"
+    active-text-color="#04816A"
+  >
     <el-menu-item index="/">
-      <img src="~/assets/img/home-alt.svg" width="30" alt="">
+      <img src="~/assets/img/home-alt.svg" width="30" alt />
       <span>Home</span>
     </el-menu-item>
 
     <el-submenu index="base-sub">
       <template slot="title">
-        <img src="~/assets/img/apps.svg" width="30" alt="">
+        <img src="~/assets/img/apps.svg" width="30" alt />
         <span>Base</span>
       </template>
       <el-menu-item index="/base">Base</el-menu-item>
@@ -19,7 +25,7 @@
 
     <el-submenu index="atoms-sub">
       <template slot="title">
-        <img src="~/assets/img/atom.svg" width="30" alt="">
+        <img src="~/assets/img/atom.svg" width="30" alt />
         <span>Atoms</span>
       </template>
       <el-menu-item index="/atoms">Atoms</el-menu-item>
@@ -32,11 +38,12 @@
       <el-menu-item index="/atoms/list">List</el-menu-item>
       <el-menu-item index="/atoms/forms">Forms</el-menu-item>
       <el-menu-item index="/atoms/spinners">Spinners</el-menu-item>
+      <el-menu-item index="/atoms/modal">Modal</el-menu-item>
     </el-submenu>
 
     <el-submenu index="molecules-sub">
       <template slot="title">
-        <img src="~/assets/img/cell.svg" width="30" alt="">
+        <img src="~/assets/img/cell.svg" width="30" alt />
         <span>Molecules</span>
       </template>
       <el-menu-item index="/molecules">Molecules</el-menu-item>
@@ -47,7 +54,7 @@
 
     <el-submenu index="organisms-sub">
       <template slot="title">
-        <img src="~/assets/img/web-grid.svg" width="30" alt="">
+        <img src="~/assets/img/web-grid.svg" width="30" alt />
         <span>Organisms</span>
       </template>
       <el-menu-item index="/organisms">Organisms</el-menu-item>
@@ -57,7 +64,7 @@
 
     <el-submenu index="templates-sub">
       <template slot="title">
-        <img src="~/assets/img/window-maximize.svg" width="30" alt="">
+        <img src="~/assets/img/window-maximize.svg" width="30" alt />
         <span>Templates</span>
       </template>
       <el-menu-item index="/templates">Templates</el-menu-item>
@@ -65,49 +72,46 @@
     </el-submenu>
 
     <menu-toggle></menu-toggle>
-
   </el-menu>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import MenuToggle from '@/components/MenuToggle.vue'
+import { mapState } from "vuex";
+import MenuToggle from "@/components/MenuToggle.vue";
 
 const debounce = (func, wait, immediate) => {
   let timeout;
   return function() {
-    let context = this, args = arguments,
-    later = function() {
-      timeout = null
-      if (!immediate) func.apply(context, args)
-    },
-    call_now = immediate && !timeout
-    clearTimeout(timeout)
-    timeout = setTimeout(later, wait)
-    if (call_now) func.apply(context, args)
-  }
-}
+    let context = this,
+      args = arguments,
+      later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      },
+      call_now = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (call_now) func.apply(context, args);
+  };
+};
 
 export default {
-  name: 'app-sidebar',
+  name: "app-sidebar",
   components: {
-    'menu-toggle': MenuToggle
+    "menu-toggle": MenuToggle
   },
-  computed: mapState([
-    'sidebarCollapsed',
-    'isMobile'
-  ]),
+  computed: mapState(["sidebarCollapsed", "isMobile"]),
   methods: {
     onResize: function() {
-      this.$store.commit('changeIsMobile', window.innerWidth < 992)
+      this.$store.commit("changeIsMobile", window.innerWidth < 992);
     }
   },
-  mounted: function () {
-    this.onResize()
-    window.addEventListener('resize', debounce(this.onResize, 250))
+  mounted: function() {
+    this.onResize();
+    window.addEventListener("resize", debounce(this.onResize, 250));
   },
-  beforeDestroy: function () {
-    window.removeEventListener('resize', this.onResize)
+  beforeDestroy: function() {
+    window.removeEventListener("resize", this.onResize);
   }
 };
 </script>
--- static/js/card-toggle.js
+++ static/js/card-toggle.js
@@ -1,12 +1,11 @@
 function initCardToggle() {
-  var cards, parent;
+  var button, parent;
+  button = document.querySelectorAll('[data-card-toggle="true"]');
 
-  cards = document.getElementsByClassName("card-toggle-button");
-
-  for (var i = 0; i < cards.length; i++) {
-    cards[i].addEventListener("click", function (e) {
+  for (var i = 0; i < button.length; i++) {
+    button[i].addEventListener("click", function (e) {
       parent = this.parentNode.parentNode;
-      toggle(cards[i], parent)
+      toggle(button[i], parent)
     });
   }
 }
@@ -14,16 +13,16 @@ function initCardToggle() {
 function toggle(toggleBtn, card) {
   var content = card.getElementsByClassName("card-expandable-content")[0];
   if (!card.classList.contains('open')) {
-    // card.classList.add('open');
-    // content.style.height = "auto";
-    // var height = content.offsetHeight + "px";
-    // content.style.height = "0px";
-    // setTimeout(function() {
-    //   content.style.height = height;
-    // }, 0);
+    card.classList.add('open');
+    content.style.height = "auto";
+    var height = content.offsetHeight + "px";
+    content.style.height = "0px";
+    setTimeout(function () {
+      content.style.height = height;
+    }, 0);
   } else {
     content.style.height = "0px";
-    content.addEventListener('transitionend', function() {
+    content.addEventListener('transitionend', function () {
       card.classList.remove('open');
     }, { once: true });
   }
