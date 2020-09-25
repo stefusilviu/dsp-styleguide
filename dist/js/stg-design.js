7daacdd23c14a54bfb9a0885f2e1dc261e6e9f6a
Nicoleta Ionescu <nionescu@mcclatchyservices.com>
2020-09-25 16:39:32 +0300

    MPP-3859 Replace collapse and toggle password buttons with Bootstrap 4 functionalities

--- dist/js/stg-design.js
+++ dist/js/stg-design.js
@@ -22,15 +22,39 @@ document.addEventListener('click', function (event) {
 });
 
 function initPasswordSwitch(element) {
-  var parent;
-  parent = element.parentNode;
-  element.classList.toggle('visible');
-  if (element.classList.contains('visible')) {
-    parent.getElementsByTagName("input")[0].setAttribute("type", "text");
-  } else {
-    parent.getElementsByTagName("input")[0].setAttribute("type", "password");
-  }
+    var parent;
+    parent = element.parentNode;
+    element.classList.toggle('visible');
+    if (element.classList.contains('visible')) {
+        parent.getElementsByTagName("input")[0].setAttribute("type", "text");
+    } else {
+        parent.getElementsByTagName("input")[0].setAttribute("type", "password");
+    }
 }
+
+(function () {
+    //'[data-toggle="password-or-text"]'
+    //this._triggerArray = [].slice.call(document.querySelectorAll("input[data-toggle=\"password-or-text\"]"));
+    var toggleList = document.querySelectorAll('input[data-toggle="password-or-text"]');
+
+    for (var i = 0; i < toggleList.length; i++) {
+        (function () {
+            var passChangeElement = document.createElement("p");
+            var targetElement = toggleList[i];
+            passChangeElement.className = "password-or-text";
+            passChangeElement.addEventListener('click', function (event) {
+                if (targetElement.getAttribute("type") === "password") {
+                    targetElement.setAttribute("type", "text");
+                    targetElement.classList.add("visible");
+                } else {
+                    targetElement.setAttribute("type", "password");
+                    targetElement.classList.remove("visible");
+                }
+            });
+            targetElement.parentNode.append(passChangeElement);
+        })();
+    }
+})();
 
 function initStgSelect() {
   var selectInputs, i, j, selElmnt, selectSelected, optionsList, optionItem;
--- static/js/password-switch.js
+++ static/js/password-switch.js
@@ -1,10 +1,34 @@
 function initPasswordSwitch(element) {
-  var parent;
-  parent = element.parentNode;
-  element.classList.toggle('visible');
-  if (element.classList.contains('visible')) {
-    parent.getElementsByTagName("input")[0].setAttribute("type", "text");
-  } else {
-    parent.getElementsByTagName("input")[0].setAttribute("type", "password");
-  }
+    var parent;
+    parent = element.parentNode;
+    element.classList.toggle('visible');
+    if (element.classList.contains('visible')) {
+        parent.getElementsByTagName("input")[0].setAttribute("type", "text");
+    } else {
+        parent.getElementsByTagName("input")[0].setAttribute("type", "password");
+    }
 }
+
+(function () {
+    //'[data-toggle="password-or-text"]'
+    //this._triggerArray = [].slice.call(document.querySelectorAll("input[data-toggle=\"password-or-text\"]"));
+    var toggleList = document.querySelectorAll('input[data-toggle="password-or-text"]');
+
+    for (var i = 0; i < toggleList.length; i++) {
+        (function () {
+            var passChangeElement = document.createElement("p");
+            var targetElement = toggleList[i];
+            passChangeElement.className = "password-or-text";
+            passChangeElement.addEventListener('click', function (event) {
+                if (targetElement.getAttribute("type") === "password") {
+                    targetElement.setAttribute("type", "text");
+                    targetElement.classList.add("visible");
+                } else {
+                    targetElement.setAttribute("type", "password");
+                    targetElement.classList.remove("visible");
+                }
+            });
+            targetElement.parentNode.append(passChangeElement);
+        })();
+    }
+})();
